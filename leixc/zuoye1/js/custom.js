4ec79a1185c8eb39a9fcfa4c3936ecb38adb897a
ptrdu <prdupc@gmail.com>
2016-02-03 01:31:27 -0800

    new feature:add the test page, design the framework of the webapp

--- leixc/zuoye1/js/custom.js
+++ leixc/zuoye1/js/custom.js
@@ -137,4 +137,89 @@ Request.prototype.end = function(callback) {
 };
 
 /******************************the render of the views**********************************/
+function VM(opts) {
+    this.opts = opts;
+}
+VM.prototype.data = function(key, value) {
 
+};
+VM.prototype.render = function(id, template) {
+
+};
+VM.prototype.create = function(fn) {
+
+};
+VM.prototype.watch = function(obj, fn) {
+
+};
+VM._init = function() {
+
+};
+/******************************the router of the page***********************************/
+function Router() {
+    this.routeMap = {};
+    this.vm = {};
+}
+
+Router.prototype.set = function(hash, vm) {
+    this.routeMap[hash] = vm;
+    return this;
+};
+
+Router._init = function() {
+    var self = this;
+    if(typeof window.location === 'undefined'){
+        console.log('need work in browser');
+        return;
+    }else{
+        if(typeof window.onhashchange !== 'undefined') {
+            window.onhashchange = function() {
+                if(self.routeMap[window.location.hash]) {
+                    self.vm = self.routeMap[window.location.hash];
+                }
+            }
+        }else{
+            var oldHash = window.location.hash;
+            var newHash;
+            setInterval(function(){
+                newHash = window.location.hash;
+                oldHash = newHash;
+                if(self.routeMap[newHash]) self.vm = self.routeMap[newHash];
+                else console.log('please add the correct route');
+            },100);
+        }
+    }
+};
+/*************************************************the start of the webapp******************************/
+/*
+<div class="m-cont f-cfb">
+<input type="text" name="title" class="c-title" value="日志标题" />
+<textarea name="content" class="c-bcon">这里可以写日志哦～</textarea>
+<input type="button" name="clear" value="清空" class="c-clr f-rf" />
+<input type="submit" name="submit" value="发布" class="c-sbt f-rf" />
+</div>
+<div class="m-bhist">
+<ul>
+<li><input type="checkbox" name="history_item" value="1" /><a href="#" title="" class="c-iname">dwr+struts+spring+hibemate简单配置</a><div class="c-action f-rf"><a href="#edit"	class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
+<p class="c-idetail"><span class="c-time">2010-06-09 13:44:38 </span><span>阅读23</span><span>评论0</span></p></li>
+
+<li><input type="checkbox" name="history_item" value="2" /><a href="#" title="" class="c-iname">dwr+struts+spring的简单配置</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
+<p class="c-idetail"><span class="c-time">2010-06-07 18:12 </span><span>阅读56</span><span>评论0</span></p>
+</li>
+<li><input type="checkbox" name="history_item" value="3" /><a href="#" title="" class="c-iname">js高性能原则（转载)</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
+<p class="c-idetail"><span class="c-time">2010-06-23 11:36:00 </span><span>阅读11</span><span>评论0</span></p>
+</li>
+<li><input type="checkbox" name="history_item" value="4" /><a href="#" title="" class="c-iname">dwr+spring+hibemate简单配置</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
+<p class="c-idetail"><span class="c-time">2010-06-07 19:20:00 </span><span>阅读48</span><span>评论0</span></p>
+</li>
+<li><input type="checkbox" name="history_item" value="5" /><a href="#" title="" class="c-iname">dwr的简单配置</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
+<p class="c-idetail"><span class="c-time">2010-06-07 14:57:21 </span><span>阅读48</span><span>评论0</span></p>
+</li>
+<li><input type="checkbox" name="all_items" value="a" id="all_items" />全选<input type="button" name="delete" id="delete" class="c-btn-del" value="删除" /></li>
+</ul>
+    <!-- ‘更多’下拉菜单 -->
+<ul class="c-more-actions f-hide">
+<li>删除</li>
+<li>置顶</li>
+</ul>
+</div>*/
