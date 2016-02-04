9137fc5d2b7d0229267de486c3aa6d9da7a4c657
ptrdu <prdupc@gmail.com>
2016-02-04 00:41:32 -0800

    new feature: change to the simplest way

--- leixc/zuoye1/js/custom.js
+++ leixc/zuoye1/js/custom.js
@@ -140,19 +140,50 @@ Request.prototype.end = function(callback) {
 function VM(opts) {
     this.opts = opts;
 }
+/**
+ * set the data of this vm
+ * @param key
+ * @param value
+ */
 VM.prototype.data = function(key, value) {
 
 };
+/**
+ * render the page according to the data and teh view
+ * @param id
+ * @param template
+ */
 VM.prototype.render = function(id, template) {
 
 };
 VM.prototype.create = function(fn) {
 
 };
+/**
+ * the directives of the View model
+ * @param name：the name of this directive, like ng-repeat
+ * @param el: the dom element contains this directive
+ * @param fn: the function deal with the dom element
+ * @param arg: arguments of this directives, like ng-repeat="key in object"
+ */
+VM.prototype.setDiretcvite = function(name, el, fn, arg){
+
+};
 VM.prototype.watch = function(obj, fn) {
+    object.keys().forEach(function(key) {
+
+    });
+};
+VM._transclude = function() {
+    var opts = this.opts;
+    var view = opts.view;
+    var needAppend = document.createDocumentFragment();
 
 };
 VM._init = function() {
+
+};
+var viewToDocumentFragement = function(view) {
 
 };
 /******************************the router of the page***********************************/
@@ -191,35 +222,56 @@ Router._init = function() {
     }
 };
 /*************************************************the start of the webapp******************************/
-/*
-<div class="m-cont f-cfb">
-<input type="text" name="title" class="c-title" value="日志标题" />
-<textarea name="content" class="c-bcon">这里可以写日志哦～</textarea>
-<input type="button" name="clear" value="清空" class="c-clr f-rf" />
-<input type="submit" name="submit" value="发布" class="c-sbt f-rf" />
-</div>
-<div class="m-bhist">
-<ul>
-<li><input type="checkbox" name="history_item" value="1" /><a href="#" title="" class="c-iname">dwr+struts+spring+hibemate简单配置</a><div class="c-action f-rf"><a href="#edit"	class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
-<p class="c-idetail"><span class="c-time">2010-06-09 13:44:38 </span><span>阅读23</span><span>评论0</span></p></li>
 
-<li><input type="checkbox" name="history_item" value="2" /><a href="#" title="" class="c-iname">dwr+struts+spring的简单配置</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
-<p class="c-idetail"><span class="c-time">2010-06-07 18:12 </span><span>阅读56</span><span>评论0</span></p>
-</li>
-<li><input type="checkbox" name="history_item" value="3" /><a href="#" title="" class="c-iname">js高性能原则（转载)</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
-<p class="c-idetail"><span class="c-time">2010-06-23 11:36:00 </span><span>阅读11</span><span>评论0</span></p>
-</li>
-<li><input type="checkbox" name="history_item" value="4" /><a href="#" title="" class="c-iname">dwr+spring+hibemate简单配置</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
-<p class="c-idetail"><span class="c-time">2010-06-07 19:20:00 </span><span>阅读48</span><span>评论0</span></p>
-</li>
-<li><input type="checkbox" name="history_item" value="5" /><a href="#" title="" class="c-iname">dwr的简单配置</a><div class="c-action f-rf"><a href="#edit" class="c-edit">编辑</a><a href="#more" class="c-more">更多<span class="c-icon-down"></span></a></div>
-<p class="c-idetail"><span class="c-time">2010-06-07 14:57:21 </span><span>阅读48</span><span>评论0</span></p>
-</li>
-<li><input type="checkbox" name="all_items" value="a" id="all_items" />全选<input type="button" name="delete" id="delete" class="c-btn-del" value="删除" /></li>
-</ul>
-    <!-- ‘更多’下拉菜单 -->
-<ul class="c-more-actions f-hide">
-<li>删除</li>
-<li>置顶</li>
-</ul>
-</div>*/
+var logShowView =
+    "<div class='m-bhist'>" +
+    "<ul>" +
+    "<li>" +
+    "<input type='checkbox' name='history_item' value='1' />" +
+    "<a href='#' title='' class='c-iname'>dwr+struts+spring+hibemate简单配置</a>" +
+    "<div class='c-action f-rf'><a href='#edit'	class='c-edit'>编辑</a><a href='#more' class='c-more'>更多<span class='c-icon-down'></span></a></div>" +
+    "<p class='c-idetail'><span class='c-time'>2010-06-09 13:44:38 </span><span>阅读23</span><span>评论0</span></p>" +
+    "</li>" +
+    "<li><input type='checkbox' name='all_items' value='a' id='all_items' />全选<input type='button' name='delete' id='delete' class='c-btn-del' value='删除' /></li>" +
+    "</div>";
+var logView = logCreateView + logShowView;
+
+/***********************************************the simple implementation of the webapp****************************************/
+/**
+ * 还是写最简单的吧。。。。
+ */
+
+window.onhashchange = function() {
+    if(window.location.hash === 'log') {
+        //do something
+        var url = ''; //the url get the data form the server
+        var data;
+        new Request()
+            .get(url)
+            .end(function(err, res) {
+                if(err !== null) {
+                    console.log(err);
+                }else{
+                    // var data = res.data; get the return data;
+                    var data = []; //the example
+                    var mainDom = document.getElementById('main'); //need to set a main block, all the elements are render in this block
+                    var logCreateView =
+                        "<div class='m-cont f-cfb'>" +
+                        "<input type='text' name='title' class='c-title' value='日志标题' />" +
+                        "<textarea name='content' class='c-bcon'>这里可以写日志哦～</textarea>" +
+                        "<input type='button' name='clear' value='清空' class='c-clr f-rf' />" +
+                        "<input type='submit' name='submit' value='发布' class='c-sbt f-rf' />" +
+                        "</div>";
+                    mainDom.innerHTML(logCreateView);
+                    var liList = document.createDocumentFragment();
+                    data.forEach(function(item) {
+                        
+                    });
+                }
+            });
+    }else if (window.location.hash === 'label'){
+        //do something
+    }else{
+        //do something
+    }
+};
