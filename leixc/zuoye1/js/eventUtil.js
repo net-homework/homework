1ec3e4a97451823d59443bf3acffc163c8ddaf76
xuechunL <xuechunlei922@gmail.com>
2016-02-05 00:16:08 +0800

    add dropdown list feature.

---
+++ leixc/zuoye1/js/eventUtil.js
@@ -0,0 +1,47 @@
+//Event Utilities :add remove 
+var EventUtil = {
+	addHandler: function (element, type, handler) {
+		 /* add */
+		 if(element.addEventListener) {
+		 	element.addEventListener(type, handler, false);
+		 }else if (element.attachEvent) {//IE 
+		 	element.attachEvent('on'+type,handler);
+		 }else {
+		 	element['on'+type] = handler;
+		 }
+	},
+	removeHandler: function (element, type, handler) {
+		 /* remove */
+		 if (element.removeEventListener) {
+		 	element.removeEventListener(type, handler, false);
+		 }else if (element.detachEvent) {
+		 	element.detachEvent('on'+type, handler);
+		 }else {
+		 	element['on'+type] = null;
+		 }
+	},
+	getEvent: function (event) {
+		 /* get event object  */
+		 return event ? event : window.event;
+	},
+	getTarget: function (event) {
+		 /* get the target of a event... */ 
+		 return event.target || event.srcElement;
+	},
+	preventDefaultE: function (event) {
+		 /* prevent the default action of the browser... */
+		 if(event.preventDefault){
+		 	event.preventDefault();
+		 }else {
+		 	event.returnValue = false;
+		 }
+	},
+	stopPropagationE: function (event) {
+		 /* stop the event propagation or event bubble... */ 
+		 if(event.stopPropagation){
+		 	event.stopPropagation();
+		 }else {
+		 	event.cancelBubble = true;
+		 }
+	}
+};
