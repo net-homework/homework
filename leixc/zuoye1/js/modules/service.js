6f8ce1b1e92c9e19cb2f2dc62894591c56382386
Ming Xu <gmbody7@gmail.com>
2016-02-25 00:15:36 -0800

    finish server function

--- leixc/zuoye1/js/modules/service.js
+++ leixc/zuoye1/js/modules/service.js
@@ -4,13 +4,13 @@
 //参数
 var setting = {
     base_url:'https://192.168.144.11',
-    get_blog_url : '/api/getblogs',
-    delete_blog_url:'/api/deleteBlogs?id=',    //delete_blog_url + blog1_id&blog2_id
-    add_blog_url:'/api/addBlog?blog=',
-    edit_blog_url:'/api/editBlog?blog=',
-    top_blog_url : '/api/topBlog?id=',
+    get_blogs_url : '/api/getblogs',
+    delete_blogs_url:'/api/deleteBlogs',    //delete_blog_url + blog1_id&blog2_id
+    add_blog_url:'/api/addBlog',
+    edit_blog_url:'/api/editBlog',
+    top_blog_url : '/api/topBlog',
     untop_blog_url:"/api/untopBlog",
-    get_friends_latest_blog_url:'/api/getFriendsLatestBlogs?userid='
+    get_friends_latest_blog_url:'/api/getFriendsLatestBlogs'
 };
 
 var util = {
@@ -40,12 +40,54 @@ var util = {
 };
 
 var server = {
-    get_blog : function(){
-        var url = setting.base_url + setting.get_blog_url;
+    get_blogs : function(callback){
+        var url = setting.base_url + setting.get_blogs_url;
         var data = {};
-        util.$post(url,data,callback);
-
+        util.$get(url,data,callback);
     },
+    delete_blogs : function(ids, callback){
+        var url = setting.base_url + setting.delete_blogs_url
+        var ids_string = ids.join('&');
+        var data = {
+            id:ids_string
+        };
+        util.$get(url,data,callback);
+    },
+    add_blog : function(blog, callback){
+        var url = setting.base_url + setting.add_blog_url;
+        var data = {
+            blog:blog
+        };
+        util.$get(url,data,callback);
+    },
+    edit_blog : function(blog, callback){
+        var url = setting.base_url + setting.edit_blog_url;
+        var data = {
+            blog:blog
+        };
+        util.$get(url,data,callback);
+    },
+    top_blog : function(id, callback){
+        var url = setting.base_url + setting.top_blog_url;
+        var data = {
+            id:id
+        };
+        util.$get(url,data,callback);
+    },
+    untop_blog : function(id, callback){
+        var url = setting.base_url + setting.untop_blog_url;
+        var data = {
+            id:id
+        };
+        util.$get(url,data,callback);
+    },
+    get_friends_latest_blog : function(userid, callback){
+        var url = setting.base_url + setting.untop_blog_url;
+        var data = {
+            userid:userid
+        };
+        util.$get(url,data,callback);
+    }
 };
 
 var $obj = {
